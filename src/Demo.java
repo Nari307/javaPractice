4d9b57a34e727b5eb920cad45ad91ef201c8635e
Nari <narayanach307@example.com>
2025-05-25 18:06:10 +0530

    pllindrome cahnges

--- src/Demo.java
+++ src/Demo.java
@@ -5,12 +5,29 @@ import java.util.Collections;
 public class Demo {
     public  static  void  main(String[] args){
 
+        //palindrome
+//        String str="madam";
+        //login function
+//        String str1="";
+//        for(int i=str.length()-1;i>=0;i--){
+//            str1=str1+str.charAt(i);
+//        }
+//        if(str.equals(str1)){
+//            System.out.print(str1);
+//            System.out.print("Given String is a palindrome");
+//        }
+//        else {
+//            System.out.print("Give string is a not a palindrome");
+//        }
+        //inbuiltmethod
+//        String rev=new StringBuilder(str).reverse().toString();
+//        System.out.print(str.equals(rev));
 
 
         //Reverse a String
 //        String str="Narayana";
 //        String rev="";
-//        //logic function
+       //logic function
 //        for(int i=str.length()-1; i>=0;i--){
 //            rev=rev+str.charAt(i);
 //        }
@@ -30,8 +47,6 @@ public class Demo {
 //for(int c:b){
 //    System.out.print(c+",");
 //}
-
-//
 //for(int b :a){
 //    System.out.println(b);
 //}
