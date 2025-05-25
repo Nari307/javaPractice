6403fa86aef4fae9b30ac3b4b08cb2b34269c6bc
Nari <narayanach307@example.com>
2025-05-25 17:51:54 +0530

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Demo {
+    public  static  void  main(String[] args){
+
+
+
+        //Reverse a String
+//        String str="Narayana";
+//        String rev="";
+//        //logic function
+//        for(int i=str.length()-1; i>=0;i--){
+//            rev=rev+str.charAt(i);
+//        }
+//        System.out.print(rev);
+//        //inbuilt method
+//        StringBuilder rev2=new StringBuilder(str).reverse();
+//        System.out.print(rev2);
+
+//System.out.println("hello user");
+//
+//int a[] ={2,53,3,25,4};
+//
+//int b[];
+//
+//b=Arrays.stream(a).sorted().toArray();
+//
+//for(int c:b){
+//    System.out.print(c+",");
+//}
+
+//
+//for(int b :a){
+//    System.out.println(b);
+//}
+}
+}
+
