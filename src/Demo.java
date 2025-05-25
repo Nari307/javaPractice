63ff058788701248fd9f84c057e1da7f9dc52e78
Nari <narayanach307@example.com>
2025-05-25 22:01:32 +0530

    merge two arrays

--- src/Demo.java
+++ src/Demo.java
@@ -3,6 +3,25 @@ import java.util.Arrays;
 public class Demo {
     public  static  void  main(String[] args){
 
+        //merge two arrays
+//        int[] arr1={1,3,5};
+//        int[] arr2={2,4,6};
+//        int[] merge=new int[arr1.length+arr2.length];
+        //custom functionality
+//        for(int i=0;i<arr1.length;i++){
+//            merge[i]=arr1[i];
+//        }
+//        for(int j=0;j<arr2.length;j++){
+//            merge[arr1.length+j]=arr2[j];
+//        }
+//        System.out.print(Arrays.toString(merge));
+
+        //inbuilt fuctions
+//        System.out.print(Arrays.toString(merge));
+//        System.arraycopy(arr1,0,merge,0,arr1.length);
+//        System.arraycopy(arr2,0,merge,arr1.length,arr2.length);
+//        System.out.print(Arrays.toString(merge));
+
         //sort an array
 //        int[] arr={23,345,32,54,53};
 //        int[] sort={};
